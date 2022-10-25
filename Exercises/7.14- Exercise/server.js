ebc4087adf111fa81d7a32561fd4964d2e517bdf
Nnanna John <nacojohn@gmail.com>
2022-10-25 20:22:10 +0100

    completed exercise 1

---
+++ Exercises/7.14- Exercise/server.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Course = require('./models/Course');
+
+mongoose.connect('mongodb://localhost:27017/mongo-exercises')
+        .then(res => console.log('connected to mongodb'))
+        .catch(errror => console.log(errror));
+
+async function getCourses() {
+    const courses = await Course
+                                .find({ tags: { $in: ['backend'] } })
+                                .sort({ name: -1 })
+                                .select({ name: 1, author: 1 });
+
+    console.log(courses);
+}
+
+getCourses();
